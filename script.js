e2bb2c0f9ef92d74f6285ea58632fc68c0e0f6e9
Korbin Sargent <korbinksargent@gmail.com>
2021-08-17 00:23:17 -0600

    Added wind speed data to dashboard

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ $(document).ready(function () {
   var apiKey = "514635bbd4899a3f7f9ebbad1a4e61e9";
   var day = moment().format("dddd");
   var date = moment().format("l");
+
+  //cities parsed from local storage. If not cities recently searched, cities is set to an empty array
   var cities = JSON.parse(localStorage.getItem("cities")) || [];
+
+  //Click event that fires when user inputs a city to search
 
   $("#submitBtn").on("click", function (e) {
     e.preventDefault();
@@ -12,13 +16,21 @@ $(document).ready(function () {
     futureWeather(cityName);
   });
 
+  //API call to gather current weather
+
   function currentWeather(cityName) {
     $.ajax({
       method: "GET",
       url: `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`,
       dataType: "json",
     }).then(function (response) {
-      console.log(response);
+      //   console.log(response);
+      //converts to Miles per hour
+      var windSpeedMPS = response.wind.speed;
+      var windSpeedMPH = windSpeedMPS * 2.237;
+      var wind = windSpeedMPH.toFixed(0);
+
+      //Adds searched city to local storage
       if (cities.indexOf(cityName) === -1) {
         cities.push(response.name);
       }
@@ -44,11 +56,15 @@ $(document).ready(function () {
           response.weather[0].icon
         }.png"></div>
         <h4>Temperature: ${response.main.temp.toFixed(0)}°F</h4>
+        <h4> Humidity: ${response.main.humidity}%</h4> 
         <h4 id="uv"></h4>
+        <h4> Wind Speed: ${wind} mph</h4>
       </div>`);
 
       var lat = response.coord.lat;
       var lon = response.coord.lon;
+
+      //API call to get UV index data
 
       $.ajax({
         method: "GET",
@@ -56,7 +72,7 @@ $(document).ready(function () {
         dataType: "json",
       }).then(function (response) {
         const uv = response.value.toFixed(0);
-        console.log(uv);
+        // console.log(uv);
         $("#uv").html(
           "UV Index: " +
             '<span class="badge badge-light text-white" id="uvColor">' +
@@ -74,16 +90,21 @@ $(document).ready(function () {
     });
   }
 
-  function futureWeather(cityName) {
-    // $("#fiveDayForecast").show();
+  //API call to get 5 day forecast daya
 
+  function futureWeather(cityName) {
     $.ajax({
       method: "GET",
       url: `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`,
       dataType: "json",
     }).then(function (response) {
+      //display 5 day forecast data
       $("#fiveDayForecast").empty();
       var forecast = response.list;
+      console.log(forecast);
+      //   var futureWindSpeedMPS = forecast.wind.speed;
+      //   var futureWindSpeedMPH = futureWindSpeedMPS * 2.237;
+      //   var futureWind = futureWindSpeedMPH.toFixed(0);
       for (let i = 0; i < forecast.length; i = i + 8) {
         $("#fiveDayForecast").append(`
         <div class="col mb-4">
@@ -98,6 +119,9 @@ $(document).ready(function () {
                     <p class="card-text">Temp: ${forecast[i].main.temp.toFixed(
                       0
                     )}°F</p>
+                    <p class="card-text">Wind Speed: ${(
+                      forecast[i].wind.speed * 2.237
+                    ).toFixed(0)} mph</p>
                     <p class="card-text">Humidity: ${
                       forecast[i].main.humidity
                     }%</p>
